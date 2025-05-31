95794adc6efe62fedcc454f02fbcbeaa28dd3f8b
facristiano <cristianof.azevedo@gmail.com>
2025-05-31 00:05:27 -0300

    Update aprimoramentos.py

--- draw/aprimoramentos.py
+++ draw/aprimoramentos.py
@@ -39,3 +39,23 @@ def load_position_data(n_bs=None, n_s=None, raw_data=None):
     uey_off_data = covert_to_km(uey_off_data)
 
     return uex_data, uey_data, bsx_data, bsy_data,uex_off_data, uey_off_data, bs_index_data
+
+def load_distance_data(bs_ue_list=None, simulation_list=None, raw_data=None):
+    distances = []
+    distance=[]
+    for i in bs_ue_list:
+         distance=[]
+         for t in simulation_list:
+                ue_position = raw_data[i][t]['ue_position']
+                ue_dim = len(ue_position)
+                for ue in range(0,ue_dim):
+                       df = raw_data[i][t]['ue_bs_table']['bs_index']
+                       bs_index = df.loc[ue]
+                       if bs_index != -1:
+                               ue_x = ue_position[ue][0]
+                               ue_y = ue_position[ue][1]
+                               bs_x = raw_data[i][t]['bs_position'][0][0][bs_index][0]
+                               bs_y = raw_data[i][t]['bs_position'][0][0][bs_index][1]
+                               distance = convert_to_km(math.sqrt((bs_x - ue_x) ** 2 + (bs_y - ue_y) ** 2)
+                               distances.append(distance)
+    return distances
