09f3e182639524f76ffc9038dc4e90fbf60814ac
facristiano <cristianof.azevedo@gmail.com>
2025-05-30 23:55:07 -0300

    Update aprimoramentos.py

--- draw/aprimoramentos.py
+++ draw/aprimoramentos.py
@@ -1,13 +1,41 @@
 def covert_to_km (data=None):
   data = (np.array(data)*30)/1000
   return data
-  
 
-def load_media_data(axis=None, bs_ue_list=None, simulation_list=None, raw_data=None):
+def load_position_data(n_bs=None, n_s=None, raw_data=None):
+    uex_data=[]
+    uey_data=[]
+    bsx_data=[]
+    bsy_data=[]
+    uex_off_data=[]
+    uey_off_data=[]
+    bs_index_data=[]
+    i = n_bs
+    t = n_s
+    ue_position = raw_data[i][t]['ue_position']
+    ue_dim = len(ue_position)
+    for ue in range(0,ue_dim):
+            df = raw_data[i][t]['ue_bs_table']['bs_index']
+            bs_index = df.loc[ue]
+            ue_x = ue_position[ue][0]
+            ue_y = ue_position[ue][1]
+            bs_x = raw_data[i][t]['bs_position'][0][0][bs_index][0]
+            bs_y = raw_data[i][t]['bs_position'][0][0][bs_index][1]
+            if bs_index != -1:
+                 bs_index_data.append(bs_index)
+                 uex_data.append(ue_x)
+                 uey_data.append(ue_y)
+                 bsx_data.append(bs_x)
+                 bsy_data.append(bs_y)
+            else:
+                 uex_off_data.append(ue_x)
+                 uey_off_data.append(ue_y)
 
-    uex_data = (np.array(uex_data)*30)/1000
-    uey_data = (np.array(uey_data)*30)/1000
-    bsx_data = (np.array(bsx_data)*30)/1000
-    bsy_data = (np.array(bsy_data)*30)/1000
-    uex_off_data = (np.array(uex_off_data)*30)/1000
-    uey_off_data = (np.array(uey_off_data)*30)/1000
+    uex_data = covert_to_km(uex_data)
+    uey_data = covert_to_km(uey_data)
+    bsx_data = covert_to_km(bsx_data)
+    bsy_data = covert_to_km(bsy_data)
+    uex_off_data = covert_to_km(uex_off_data)
+    uey_off_data = covert_to_km(uey_off_data)
+
+    return uex_data, uey_data, bsx_data, bsy_data,uex_off_data, uey_off_data, bs_index_data
